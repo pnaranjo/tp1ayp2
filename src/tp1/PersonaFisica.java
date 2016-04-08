6936538a6f483b6970841b60658c9cfa331e7ac5
Antonio Zimmerman <antoniozimmerman@gmail.com>
2016-04-08 17:39:25 -0300

    Contructores, getters y setters.

--- src/tp1/PersonaFisica.java
+++ src/tp1/PersonaFisica.java
@@ -1,6 +1,10 @@
 package tp1;
 
-public class PersonaFisica {
+import boceto_TP.Cliente;
+import boceto_TP.Domicilio;
+import boceto_TP.PersonaFisicaException;
+
+public class PersonaFisica extends Cliente {
 	
 	private final String tipoDeDocumento;
 	private final int numeroDeDocumento;
@@ -8,7 +12,20 @@ public class PersonaFisica {
 	private String estadoCivil;
 	private String conyugue;
 	
-	public PersonaFisica(String tipoDeDocumento, int numeroDeDocumento, String profesion, String estadoCivil, String conyugue) {
+	//Constructor para cliente HABILITADO y SOLTERO/A
+	public PersonaFisica(String nombre, long cuit, Domicilio domicilio, int telefono, String tipoDeDocumento, int numeroDeDocumento, String profesion) {
+		super (nombre, cuit, domicilio, telefono, true);
+		
+		this.tipoDeDocumento = tipoDeDocumento;
+		this.numeroDeDocumento = numeroDeDocumento;
+		this.profesion = profesion;
+		this.estadoCivil = "Soltero/a";
+		this.conyugue = "";
+	}
+	
+	public PersonaFisica(String nombre, long cuit, Domicilio domicilio, int telefono, boolean habilitado, String tipoDeDocumento, int numeroDeDocumento, String profesion, String estadoCivil, String conyugue) {
+		super (nombre, cuit, domicilio, telefono, habilitado);
+		
 		this.tipoDeDocumento = tipoDeDocumento;
 		this.numeroDeDocumento = numeroDeDocumento;
 		this.profesion = profesion;
@@ -16,7 +33,9 @@ public class PersonaFisica {
 		this.conyugue = conyugue;
 	}
 	
-	public PersonaFisica(String tipoDeDocumento, int numeroDeDocumento, String profesion, String estadoCivil) throws PersonaFisicaException {
+	//Constructor para PersonaFisica con estado Viudo, Divorciado o soltero. 
+	public PersonaFisica(String nombre, long cuit, Domicilio domicilio, int telefono, boolean habilitado,String tipoDeDocumento, int numeroDeDocumento, String profesion, String estadoCivil) throws PersonaFisicaException {
+		super (nombre, cuit, domicilio, telefono, habilitado);
 		this.tipoDeDocumento = tipoDeDocumento;
 		this.numeroDeDocumento = numeroDeDocumento;
 		this.profesion = profesion;
@@ -68,10 +87,6 @@ public class PersonaFisica {
 	public String getProfesion() {
 		return profesion;
 	}
-	
-	
-	
-	
+	}
 
-}
-
+			
