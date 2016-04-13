50062f57cc1bb62452e77f9b2dbc27ce1797aebe
pnaranjo <pablo.naranjo@mercadolibre.com>
2016-04-13 01:43:57 -0300

    Clases

--- src/tp1/CajaDeAhorroEnDolares.java
+++ src/tp1/CajaDeAhorroEnDolares.java
@@ -1,6 +1,15 @@
 package tp1;
 
+import java.util.ArrayList;
+
 public class CajaDeAhorroEnDolares extends CajaDeAhorro {
+		private static double costoDeMantenimientoDolares;
+		public CajaDeAhorroEnDolares(ArrayList<PersonaFisica> titulares, double tasaDeInteres, double tipoDeCambioVigente, double costoDeMantenimiento){
+			super (titulares, tasaDeInteres, tipoDeCambioVigente);
+			CajaDeAhorroEnDolares.costoDeMantenimientoDolares = costoDeMantenimiento;
+		}
 		
-		CajaDeAhorroEnDolares()
+		public double getCostoDeManteniento(){
+			return CajaDeAhorroEnDolares.costoDeMantenimientoDolares;
+		}
 }
--- src/tp1/CajaDeAhorroEnPesos.java
+++ src/tp1/CajaDeAhorroEnPesos.java
@@ -1,5 +1,14 @@
 package tp1;
 
+import java.util.ArrayList;
+
 public class CajaDeAhorroEnPesos extends CajaDeAhorro {
 
+	public CajaDeAhorroEnPesos(ArrayList<PersonaFisica> titulares,
+			double tasaDeInteres, double tipoDeCambioVigente) {
+		super(titulares, tasaDeInteres, tipoDeCambioVigente);
+		// TODO Auto-generated constructor stub
+		
+	}
+		
 }
