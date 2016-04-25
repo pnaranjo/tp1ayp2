5d771d4274b723eb564c6f6323f223cf0c77df8e
Antonio Zimmerman <antoniozimmerman@gmail.com>
2016-04-25 00:13:33 -0300

    Excepciones vueltas a pegar

---
+++ src/excepciones/ExceptionCuitNoValido.java
@@ -0,0 +1,15 @@
+package excepciones;
+
+public class ExceptionCuitNoValido extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8015132561630225950L;
+	
+	public ExceptionCuitNoValido(String message){
+		super(message);
+	}
+	
+
+}
