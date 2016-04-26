52ea5bc2c313745f09757db5f5589da139fe5d8e
Antonio Zimmerman <antoniozimmerman@gmail.com>
2016-04-26 18:02:11 -0300

    acreditarRetenciones() genera Transacción

--- src/clases/Banco.java
+++ src/clases/Banco.java
@@ -3,38 +3,52 @@ package clases;
 import java.util.HashMap;
 import java.util.Map;
 import excepciones.MontoException;
+import excepciones.TransaccionException;
 
 public class Banco{
 
 
     public static Map<Long,CuentaComun > portfolioDeCuentas;
     public static Map<Long, Cliente> portfolioDeClientes;
-    private static double tipoDeCambioVigente = 15.0;
+    private static double tipoDeCambioVigente;
     public static long generadorCbu = 1; /*generador de CBU*/
-    private static double costoDeMantemientoPesos = 30;//RC Como todas las cuentas van a tener el mismo costo de mantenimiento
-    // para mi no tiene sentido tener costo de mant. pesos y otro dolares. Basta con que sea solam pesos. y en 
-    // CA dolares le seteamos el cost. de mant. al valorde cambio vigente. Que opinan? Ademas tuve que agregar la 
-    //palabra static al atributo.
-    private double costoDeMantemientoDolares = 2;
-    private static CuentaEspecial retenciones;//RC agregue la palabra static para poder realizar la acreditacion de retenciones de los movimientos en cc.
+    private static double costoDeMantemientoPesos;
+    private double costoDeMantemientoDolares;
+    private static CuentaEspecial retenciones;
     private CuentaEspecial mantenimiento;
     
     
+    /*
+     * Si no se le pasan parámetros
+     * Post: El banco se crea con valores por default.
+     */
     public Banco() {
         portfolioDeCuentas = new HashMap<Long, CuentaComun>();
         portfolioDeClientes = new HashMap<Long, Cliente>();
+        
+        tipoDeCambioVigente = 15.0;
+        costoDeMantemientoPesos = 30;
+        costoDeMantemientoDolares = 2;
     }
     
+    
+    /*
+     * Si se le pasan los parámetros tipoDeCambioVigente y costoDeMantemientoPesos.
+     * Post: El banco se crea con un valor calculado para costoDeMantemientoDolares
+     */
     public Banco(double tipoDeCambioVigente, double costoDeMantemientoPesos) throws MontoException{
         portfolioDeCuentas = new HashMap<Long, CuentaComun>();
         portfolioDeClientes = new HashMap<Long, Cliente>();
         validar(tipoDeCambioVigente);
         validar(costoDeMantemientoPesos);
         Banco.tipoDeCambioVigente = tipoDeCambioVigente;
-        this.costoDeMantemientoPesos = costoDeMantemientoPesos;
+        Banco.costoDeMantemientoPesos = costoDeMantemientoPesos;
         this.costoDeMantemientoDolares = costoDeMantemientoPesos/tipoDeCambioVigente;
     }
     
+    /*
+     * El monto ingresado es mayor a 0.
+     */
     public void validar(double cantidad)throws MontoException{
     	if(cantidad<=0){
     		new MontoException("El monto ingresado no es correcto. Ingrese un monto mayor que 0.");
@@ -59,15 +73,23 @@ public class Banco{
     }
     
     public void setCostoDeMantenimientoPesos (double costoDeMantenimientoPesos) {
-    	this.costoDeMantemientoPesos = costoDeMantenimientoPesos;
+    	Banco.costoDeMantemientoPesos = costoDeMantenimientoPesos;
     }
     
     public void setCostoDeMantenimientoDolares (double costoDeMantenimientoDolares) {
     	this.costoDeMantemientoDolares = costoDeMantenimientoDolares;
     }
     
-    public static void acreditarRetenciones(double monto) {
+    public static void acreditarRetenciones(long cbu, double monto, String motivo) throws TransaccionException, MontoException {
+    	Transaccion t = new Transaccion(("Cuenta origen: " + cbu), monto, motivo);
+    	retenciones.historial.add(t);
     	//TODO generar transaccion
+    	retenciones.saldo+=monto;
+    }
+    
+    public static void acreditarRetenciones(long cbu, double monto, String motivo, String observaciones) throws TransaccionException, MontoException {
+    	Transaccion t = new Transaccion(("Cuenta origen: " + cbu), monto, motivo, observaciones);
+    	retenciones.historial.add(t);
     	retenciones.saldo+=monto;
     }
     
--- src/clases/CuentaCorriente.java
+++ src/clases/CuentaCorriente.java
@@ -62,7 +62,7 @@ public class CuentaCorriente extends CuentaComun {
 		saldo += (monto - cobrarComision(monto));
 		t1 = new Transaccion("acreditar", monto, motivo);
 		historial.add(t1);
-		Banco.acreditarRetenciones(cobrarComision(monto));
+		Banco.acreditarRetenciones(this.getCbu(),cobrarComision(monto), motivo);
 		return t1.toString();
 	}
 	public String acreditar(double monto, String motivo,String observacion) throws TransaccionException, MontoException{
@@ -71,31 +71,31 @@ public class CuentaCorriente extends CuentaComun {
 		saldo += (monto - cobrarComision(monto));
 		t1 = new Transaccion("acreditar", monto, motivo,observacion);
 		historial.add(t1);
-		Banco.acreditarRetenciones(cobrarComision(monto));
+		Banco.acreditarRetenciones(this.getCbu(),cobrarComision(monto), motivo, observacion);
 		return t1.toString();
 	}
 	public String debitar(double monto, String motivo) throws TransaccionException, MontoException, DebitarException{		
 		if(monto <= 0)
 			throw new MontoException();
 		if(saldo - (monto + cobrarComision(monto)) < (-(montoSobreGiro))){
-			throw new DebitarException("La operación no se ha podido realizar, saldo insuficiente");
+			throw new DebitarException("La operaciï¿½n no se ha podido realizar, saldo insuficiente");
 		}
 		saldo -= (monto + cobrarComision(monto));
 		t1 = new Transaccion("acreditar", monto, motivo);
 		historial.add(t1);
-		Banco.acreditarRetenciones(cobrarComision(monto));
+		Banco.acreditarRetenciones(this.getCbu(),cobrarComision(monto), motivo);
 		return t1.toString();
 	}
 	public String debitar(double monto, String motivo,String observacion) throws TransaccionException, MontoException, DebitarException{
 		if(monto <= 0)
 			throw new MontoException();
 		if(saldo - (monto + cobrarComision(monto)) < (-(montoSobreGiro))){
-			throw new DebitarException("La operación no se ha podido realizar, saldo insuficiente");
+			throw new DebitarException("La operaciï¿½n no se ha podido realizar, saldo insuficiente");
 		}
 		saldo -= (monto + cobrarComision(monto));
 		t1 = new Transaccion("acreditar", monto, motivo,observacion);
 		historial.add(t1);
-		Banco.acreditarRetenciones(cobrarComision(monto));
+		Banco.acreditarRetenciones(this.getCbu(),cobrarComision(monto), motivo, observacion);
 		return t1.toString();
 	}
 }
